53c4f32aa5f52e6ae1889e034bd502ba93804a6a
htetn <htetn@ubuntu.(none)>
2014-02-28 12:14:17 -0500

    I2C test

--- hello.c
+++ hello.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 int main() {
-	printf("Hello World\n");
-	return 0;
+	char *port = "/dev/i2c-0";
+
+	int fd = open (port, O_RDWR | O_NOCTTY | O_SYNC);
+	if (fd < 0)
+	{
+        printf ("error");
+        return;
+	}
+	while (0) {
+		write (fd, "hello!\n", 7);
+	}
 }
