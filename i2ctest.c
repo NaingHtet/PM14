52f1384e0c385b2522d728f5b320c2b9e61faff0
Naing Htet <htetn@ubuntu.(none)>
2014-03-03 13:21:43 -0500

    Added i2ctest 7

--- i2ctest.c
+++ i2ctest.c
@@ -19,13 +19,15 @@ int main() {
         return;
 	}
 
- 	if (ioctl(fd, I2C_SLAVE, "G3") < 0) {
+	int addr = 0x02;
+ 	if (ioctl(fd, I2C_SLAVE, addr) < 0) {
       	printf ( "Address Error = %s\n", strerror( errno ) ); 
     	return;
   	}
 
 
 	int n;
+	/**
 	n = write (fd, "S0217R02P", 9);
 	if (n < 0)
       	printf ( "Write Error = %s\n", strerror( errno ) ); 
@@ -33,13 +35,17 @@ int main() {
     	printf ("Write succeed n = %i\n", n );
   	
   	char *buf;
-  	n = read( fd, buf, 1 );
+  	**/
+  	char buf[2];
+  	buf[0]= 'F';
+  	buf[1]= 'E';
+  	buf[2]= '\0';
+  	n = read( fd, buf, 2 );
  
   	if ( n == -1 )
       	printf ( "Error = %s\n", strerror( errno ) ); 
   	printf ( "Number of bytes to be read = %i\n", n );
   	printf ( "Buf = %s\n", buf );
- 
   	close( fd );
 
 }
